5a8ea3c46c3777cd542eae8753e2c0eec09d1aea
sabamh <66217667+saba-bot@users.noreply.github.com>
2022-10-17 12:24:09 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
-
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
-
-from app.config import mysqlconfig
+from app.config import mysqlConfig
 from app.models.item import ItemModel
 from app.models.store import StoreModel
 from app.models.user import UserModel
+from app.resources.item import Item, ItemList
+from app.resources.store import Store, StoreList
+from app.resources.user import User, UserRegister
 
-#print(mysqlconfig)
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']=mysqlconfig
-app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
-app.config["JWT_SECRTE_KEY"]="my.secrete.key"
 
-jwt=JWTManager(app)
-api=Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = mysqlConfig
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "my.secret.key"
+
+jwt = JWTManager(app)
+api = Api(app)
 
 @app.before_first_request
-def create_table():
-    pass
+def create_tables():
+    from app.db import db
+    print("inside before first request")
+    db.init_app(app)
+    db.create_all()
 
-#app.add_resource(classname,'/item')
 
-if __name__=='__main__':
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(User, '/user')
+api.add_resource(UserRegister, '/register')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
 
 mysql = {
-    'host':'localhost',
-    'user':'root',
-    'password':'admin',
-    'db':'item_store_db'
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'admin',
+    'db': 'item_store_db'
 }
 
-mysqlconfig = f"mysql+pymysql://{mysql['user']}:{mysql['password']}@{mysql['host']}/{mysql['db']}"
+mysqlConfig = f"mysql+pymysql://{mysql['user']}:{mysql['password']}@{mysql['host']}/{mysql['db']}"
