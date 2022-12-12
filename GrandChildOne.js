5fb6f83fa78472d3b8432197917d4c01a07a2d80
Ubuntu <sfjbs@vm1.lytvvod5yb3udgrt45gyjqv15b.rx.internal.cloudapp.net>
2022-12-12 12:22:56 +0000

    First jenkis

--- GrandChildOne.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GrandChildOne = () => {
-  return <div className="grand-child-1">Grand Chile One</div>;
-};
-
-export default GrandChildOne;
--- README.md
+++
@@ -1,2 +0,0 @@
-# pax02
-Mini project for the sfjbs training
